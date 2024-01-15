7efa6cf2f5e3fcb096f664a3b263dc0612e8b1ea
mariliatostesdev <mahtostes.dev@gmail.com>
2024-01-15 17:34:15 -0300

    update

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,10 +4,13 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -30,7 +33,11 @@ public class Postagem {
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
-
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	// Generating getters and setters:
 	public Long getId() {
 		return id;
@@ -63,4 +70,14 @@ public class Postagem {
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	
+	
 }
